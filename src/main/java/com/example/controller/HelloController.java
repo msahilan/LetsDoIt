5083a2411bdd0cbda8f2bdfb1588a5295bd2266a
Ahilan Santhalingam <ahilansanthalingam@ahilans-mbp.home>
2020-04-01 18:56:03 +0100

    Intial setup

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @PostMapping("/hello")
+    public String hello(){
+
+        return "Hello from Application";
+    }
+
+}
